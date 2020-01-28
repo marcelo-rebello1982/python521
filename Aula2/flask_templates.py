b0d22bcd4bfe966c7ce750e0437c2e68fd4edc28
Ivan Ferro <iaferro@gmail.com>
2020-01-28 18:10:47 -0200

    final do dia

--- Aula2/flask_templates.py
+++ Aula2/flask_templates.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    dados = {'title': 'Titulo Jinja',
+             'cabecalho': 'Eu sei programar', 'botao': False
+             }
+    # return flask.render_template('index.html', dados=dados)
+    # return flask.render_template('modelo.html')
+    return flask.render_template('principal.html')
+
+
+#  return flask.render_template('index.html', title='Titulo jinja',cabecalho='eu sei programar', botao=False)
+app.run(debug=True)
